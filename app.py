584c25a3b1700d8e70107d24eac0346d26d52be8
Dinh-Tuyen Le <dile21@student.sdu.dk>
2022-01-25 21:34:09 +0100

    Flask hello world

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
+from flask import Flask
 import random
+
+app = Flask (__name__)
+
+@app.route('/')
+def home():
+  return '<p> Hello world! </p>'
 
 def main():
   dice_sum = 0
-  dice_rolls = int(input('How many dice would you like to roll?'))
-  dice_size = int(input('How many sides are the dice?'))
+  # dice_rolls = int(input('How many dice would you like to roll?'))
+  dice_rolls =3
+  # dice_size = int(input('How many sides are the dice?'))
+  dice_size = 6
   for i in range(0,dice_rolls):
     roll = random.randint(1,dice_size)
     dice_sum = dice_sum + roll
@@ -18,3 +27,4 @@ def main():
 
 if __name__== "__main__":
   main()
+  app.run(host='0.0.0.0',port=5000)
